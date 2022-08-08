a905dd57bc6f04077a6ff1343cea3fc587073100
erun94510 <erun9414@icloud.com>
2022-08-09 01:32:27 +0900

    Initial Commit

---
+++ WeWant/WeWantApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeWantApp.swift
+//  WeWant
+//
+//  Created by Donghoon Bae on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct WeWantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
